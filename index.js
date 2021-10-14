32f5d2d4d2bde4cc79aa32053bcc3a40a957ab0b
discombobulateme <p@discombobulate.me>
2021-10-14 11:20:56 +0200

    Lint and uncomment log

--- index.js
+++ index.js
@@ -12,14 +12,18 @@ ar3 should be equal to = [1, 3, 4, 9, 6]
 let ar1 = [1, 2, 3, 4, 5];
 let ar2 = [5, 9, 6, 2];
 
-function mergeExclusive ( array1, array2 ) {
-  let exclusiveA = array1.filter( index => !array2.some( index2 => index === index2 ) );
-  let exclusiveB = array2.filter( index => !array1.some( index2 => index === index2 ) );
-  let result = exclusiveA.concat(exclusiveB)
-  console.log( result );
-  return result
+function mergeExclusive(array1, array2) {
+  let exclusiveA = array1.filter(
+    (index) => !array2.some((index2) => index === index2)
+  );
+  let exclusiveB = array2.filter(
+    (index) => !array1.some((index2) => index === index2)
+  );
+  let result = exclusiveA.concat(exclusiveB);
+  console.log(result);
+  return result;
 }
 
-mergeExclusive(ar1, ar2)
+mergeExclusive(ar1, ar2);
 
 module.exports = mergeExclusive;
